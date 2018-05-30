f93df1e1d322bda64b31fe7962a44ee21a63c83f
Oliv-S <36530788+Oliv-S@users.noreply.github.com>
2018-05-30 12:59:05 +0200

    AnonymousInnerClass
    class AnonymousInnerClass updated

---
+++ src/NestedClasses/AnonymousInnerClass.java
@@ -0,0 +1,38 @@
+package NestedClasses;
+
+public class AnonymousInnerClass {
+	
+	abstract class Inner{
+		abstract int add(int var1, int var2);
+	}
+	
+	public int calc(int var1, int var2) {
+		Inner inner = new Inner() {
+			int add(int var1, int var2) {
+				return var1+var2;
+			}
+		};
+		return inner.add(var1, var2);
+	}
+	
+	private interface InnerInterface{
+		int add(int var1, int var2);
+	}
+	public int calcWithInterface(int var1, int var2) {
+		InnerInterface inner = new InnerInterface() {
+			public int add(int var1, int var2) {
+				return var1+var2;
+			}
+		};
+		return inner.add(var1, var2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnonymousInnerClass outer = new  AnonymousInnerClass();
+		System.out.println(outer.calc(10,15));
+		System.out.println(outer.calcWithInterface(10,15));
+
+	}
+
+}
