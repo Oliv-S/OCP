7b9f91c0b5f207b8861812de08245d3a93db5b61
Oliv-S <36530788+Oliv-S@users.noreply.github.com>
2018-06-05 13:31:13 +0200

    StaticNestedClass updtae

--- src/NestedClasses/StaticNestedClass.java
+++ src/NestedClasses/StaticNestedClass.java
@@ -5,10 +5,44 @@ public class StaticNestedClass {
 	static class Nested{
 		private int var1 = 100;
 	}
+
+	static class Browser{
+		public void printName() {
+			System.out.println("Browser");;
+		}
+	}
+
+	static class FireFox extends Browser{
+		public void printName() {
+			System.out.println("FirFox");;
+		}
+	}
+
+	static class IE extends Browser{
+		@Override public void printName() {
+			System.out.println("IE");;
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Nested nested = new Nested();
 		System.out.println(nested.var1);
+		
+		Browser browser = new FireFox();
+		browser.printName();
+		
+		FireFox ff = (FireFox) browser;
+		ff.printName();
+		
+		try {
+		IE ie = (IE) browser;
+		ie.printName();
+		}
+		catch (ClassCastException e) {
+			// TODO: handle exception
+			System.out.println("Can't convert FF to IE");
+		}
 		
 	}
 
