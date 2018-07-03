0cda1343e643cf25b4dc8db0d236676074612f65
Oliv-S <36530788+Oliv-S@users.noreply.github.com>
2018-07-03 13:29:29 +0200

    Generics

---
+++ src/Generics/GenericSupertypes.java
@@ -0,0 +1,19 @@
+package Generics;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class GenericSupertypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		List<? super IOException> exceptions = new ArrayList<Exception>();
+		//exceptions.add(new Exception());
+		exceptions.add(new IOException());
+		exceptions.add(new FileNotFoundException());
+
+	}
+
+}
