06d373f565e68d4f49e678c536fc22143e8c77d1
Oliv-S <36530788+Oliv-S@users.noreply.github.com>
2018-07-05 10:01:53 +0200

    Comparator Edited

--- src/Compare/CompareTest.java
+++ src/Compare/CompareTest.java
@@ -2,6 +2,8 @@ package Compare;
 
 import java.util.*;
 
+import javax.swing.plaf.multi.MultiListUI;
+
 public class CompareTest {
 
 	public static void main(String[] args) {
@@ -18,19 +20,53 @@ public class CompareTest {
 		for (Squirrel squirrel : squirrels) {
 			System.out.println(squirrel);
 		}
-		Comparator<Squirrel> comparator = new Comparator<Squirrel>() {
-			public int compare(Squirrel o1, Squirrel o2) {
-				// TODO Auto-generated method stub
-				int result = o2.getAge()-o1.getAge();
-				return result;
-			}
-		};
-		Arrays.sort(squirrels, comparator);
+		
+//		Comparator<Squirrel> comparator = new Comparator<Squirrel>() {
+//			public int compare(Squirrel o1, Squirrel o2) {
+//				// TODO Auto-generated method stub
+//				int result = o2.getAge()-o1.getAge();
+//				return result;
+//			}
+//		};
+//		
+//		Comparator<Squirrel> multiComparator = new MultiFieldComparator();
+//		
+//		Arrays.sort(squirrels, multiComparator);
+//		System.out.println("***********");
+//		for (Squirrel squirrel : squirrels) {
+//			System.out.println(squirrel);
+//		}
+////		***************************************
+//		Arrays.sort(squirrels, new Comparator<Squirrel>() {
+//			public int compare(Squirrel s1, Squirrel s2) {
+//				return s1.getWeight()-s2.getWeight();
+//			}
+//		});
+//		
+//		System.out.println("***********");
+//		for (Squirrel squirrel : squirrels) {
+//			System.out.println(squirrel);
+//		}
+//		
+////		*****************************************
+//		Arrays.sort(squirrels, (s1,s2)->s1.getName().compareTo(s2.getName()));
+//		
+//		System.out.println("***********");
+//		for (Squirrel squirrel : squirrels) {
+//			System.out.println(squirrel);
+//		}
+		
+//		*****************************************
+		//Arrays.sort(squirrels, MultiFieldComparator.orderByAgeReverse());
+		Arrays.sort(squirrels, MultiFieldComparator::compareByAge);
+		//Arrays.sort(squirrels, Comparator.reverseOrder());
+		
 		System.out.println("***********");
 		for (Squirrel squirrel : squirrels) {
 			System.out.println(squirrel);
 		}
 		
+		Set<Squirrel> set = new TreeSet<>((s1,s2)->s1.getAge()-s2.getAge());		
 
 	}
 
--- src/Compare/MultiFieldComparator.java
+++ src/Compare/MultiFieldComparator.java
@@ -16,19 +16,24 @@ public class MultiFieldComparator implements Comparator<Squirrel> {
 		return o1.getName().compareTo(o2.getName());
 	}
 	
-	public static final Comparator<Squirrel> compareByAllFields(Squirrel o1, Squirrel o2) {
+	public static Comparator<Squirrel> orderByAge() {
 		// TODO Auto-generated method stub
-		return new Comparator<Squirrel>() {
-			public int compare(Squirrel o1, Squirrel o2) {
-				// TODO Auto-generated method stub
-				int result = o1.getAge()-o2.getAge();
-				if (result != 0) return result;
-				
-				result = o1.getWeight()-o2.getWeight();
-				if (result != 0) return result;
-				
-				return o1.getName().compareTo(o2.getName());
-			}
-		};
+		return (s1,s2)->s1.getAge()-s2.getAge();		
 	}
+	public static Comparator<Squirrel> orderByWeight() {
+		// TODO Auto-generated method stub
+		return (s1,s2)->s1.getWeight()-s2.getWeight();		
+	}
+	public static Comparator<Squirrel> orderByAgeReverse() {
+		// TODO Auto-generated method stub
+		return (s1,s2)->s2.getAge()-s1.getAge();		
+	}
+	public static Comparator<Squirrel> orderByWeightReverse() {
+		return (s1,s2)->s2.getWeight()-s1.getWeight();}
+	
+	public static int compareByAge(Squirrel s1, Squirrel s2) {
+		// TODO Auto-generated method stub
+		return s1.getAge()-s2.getAge();		
+	}
+	
 }
