d8004d3a514ec86a668e6b2cb2176c31692736cb
Oliv-S <36530788+Oliv-S@users.noreply.github.com>
2018-07-05 10:54:45 +0200

    RemoovingConditionally updated

--- src/Collections/RemoovingConditionally.java
+++ src/Collections/RemoovingConditionally.java
@@ -3,6 +3,9 @@ package Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+
+import org.apache.derby.tools.sysinfo;
 
 public class RemoovingConditionally {
 
@@ -11,14 +14,35 @@ public class RemoovingConditionally {
 		List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(10, 15, 17, 6, 3, 8, 19, 11, 15, 3, 7, 8, 3, 0, 4, 4, 7, 2));
 		List<Integer> listOfIntegersCopy1 = new ArrayList<>(listOfIntegers);
 		List<Integer> listOfIntegersCopy2 = new ArrayList<>(listOfIntegers);
+		List<Integer> listOfIntegersCopy3 = new ArrayList<>(listOfIntegers);
 		System.out.println(listOfIntegers);
+		
 		listOfIntegersCopy1.removeIf(a -> a<10 );
 		System.out.println(listOfIntegersCopy1);
 		
 		listOfIntegersCopy2.removeIf(a -> a%3!=0 );
 		System.out.println(listOfIntegersCopy2);
+
+		Predicate<Integer> pre3 = i -> i>=10;
+		listOfIntegersCopy3.removeIf(pre3);
+		System.out.println(listOfIntegersCopy3);
 		
+		System.out.println("*************************");
+		List<String> stringList = new ArrayList<>();
+		stringList.add("One");
+		stringList.add("Two");
+		stringList.add("Three");
+		stringList.add("Four");
+		stringList.add("Five");
+		System.out.println(stringList);
 		
+		stringList.removeIf(s -> s.startsWith("T"));
+		System.out.println(stringList);
+		
+		Predicate<String> preStr = s -> s.length()>3;
+		stringList.removeIf(preStr);
+		System.out.println(stringList);
+
 	}
 
 }
