6cfa7bb4e375ab3e57c481f72b252ce9876338d8
Oliv-S <36530788+Oliv-S@users.noreply.github.com>
2018-07-11 11:10:15 +0200

    properties

--- src/DatesTimesAndLocalization/ZooOpener.java
+++ src/DatesTimesAndLocalization/ZooOpener.java
@@ -7,18 +7,27 @@ public class ZooOpener {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(System.getProperty("user.dir"));
 		Locale usLocale = new Locale("en_US");
 		Locale frLocale = new Locale.Builder()
 				.setLanguage("fr")
 				.setRegion("FR")
 				.build();
+		printProperties(null);
 		printProperties(frLocale);
 		printProperties(usLocale);
 
 	}
 	
 	public static void printProperties(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundle.getBundle("Zoo", locale);
+		ResourceBundle resourceBundle=null;
+		if (locale!=null){
+			resourceBundle = ResourceBundle.getBundle("Zoo", locale);
+			System.out.println("Gefunden!");
+		}else {
+			resourceBundle = ResourceBundle.getBundle("Zoo");
+			System.out.println("Gefunden!");
+		}
 		System.out.println(resourceBundle.getString("hello"));
 		System.out.println(resourceBundle.getString("open"));
 	}
