0f9b8105080c9ced70fad8c33cbef87d90051260
Oliv-S <36530788+Oliv-S@users.noreply.github.com>
2018-07-11 10:47:43 +0200

    streams test

--- src/Streams/StreamsTest.java
+++ src/Streams/StreamsTest.java
@@ -1,7 +1,5 @@
 package Streams;
 
-import java.util.Optional;
-import java.util.OptionalLong;
 
 import java.util.stream.Stream;
 
@@ -9,16 +7,10 @@ public class StreamsTest {
 
 	public static void main(String[] args) {
 		Stream<String> stream = Stream.iterate("",s->s+"1");
-<<<<<<< HEAD
 
-		stream.limit(2).map(x->x+"2").forEach(System.out::println);;
+
 		stream.limit(2).map(x->x+"2").forEach(System.out::println);
-=======
-		stream.limit(2).map(x->x+"2").forEach(System.out::println);;
 
-
-		
->>>>>>> DatesTimes
 
 	}
 
